e479f0a43ec86b6824aa2c5b849d1a9bba12e559
deepanshuprabhatkhabar <deepanshu.sharmaprabhatkhabar.in>
2024-11-22 19:11:46 +0530

    updated

--- routes/party.js
+++ routes/party.js
@@ -68,7 +68,7 @@ router.get('/top-parties', async (req, res) => {
     topParties = [...topParties, {
       _id: new mongoose.Types.ObjectId(),
       party: 'Others',
-      color_code: '#FFFFFF',
+      color_code: '#1255CC',
       total_votes: 0,
       total_seat: 0,
       party_logo: null,
