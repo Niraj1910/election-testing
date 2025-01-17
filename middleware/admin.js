dcea854a000310528d1b7ea1d244831d97aaf7f4
Niraj Shaw <nirazshaw156@gmail.com>
2025-01-17 16:26:05 +0530

    feat/delhi map

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,8 +1,13 @@
 const isAdmin = (req, res, next) => {
-  if (req.session.user && (req.session.user.role === 'admin' ||  req.session.user.role === 'editor')) {
+  if (
+    req.session.user &&
+    (req.session.user.role === "admin" || req.session.user.role === "editor")
+  ) {
     next();
   } else {
-    res.status(403).json({ message: 'You are not authorized to access this resource' });
+    res
+      .status(403)
+      .json({ message: "You are not authorized to access this resource" });
   }
 };
 
